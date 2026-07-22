1c680ecac1adc4996ea9f44e573deef03fee9328
Tarun Saratkar <tarunsaratkar@gmail.com>
2026-07-23 01:34:12 +0530

    feat: connect MongoDB Atlas and configure backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,17 @@
 require("dotenv").config();
 
 const app = require("./app");
+const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
 
+const startServer = async ()=>{
+    await connectDB();
 app.listen(PORT, () => {
+
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
+};
+
+console.log(process.env.MONGODB_URI)
+startServer();
